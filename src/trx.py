6bb68cdf2cffe5290fa22f9fd4c77cdc96a9d11e
Hossein samiei <syhsamiei@gmail.com>
2023-10-06 17:15:49 +0330

    feat: add logic to trx

--- src/trx.py
+++ src/trx.py
@@ -1,6 +1,14 @@
+import time
+
 class Trx():
     def __init__(self, gas, size, id):
-        pass
+        self.gas = gas
+        self.size = size
+        self.id = id
+        self.type = type
+        self.gen_time = time.ctime()
+        self.appended_time = None
+        self.blocked_time = None
 
     def __repr__(self):
-        pass
+        return str(self.id)
