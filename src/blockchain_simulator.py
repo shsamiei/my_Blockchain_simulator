f243da0f44e27c341cb7eb1553c6b14b8f21fdcd
Hossein samiei <syhsamiei@gmail.com>
2023-10-06 16:47:21 +0330

    feat: add funnctions

---
+++ src/blockchain_simulator.py
@@ -0,0 +1,47 @@
+
+
+
+class node():
+
+    def __init__(self, node_id):
+        pass
+
+    def add_trx(self, trx):
+        pass    
+
+    def receiver(self, data, type, sender):
+        pass
+
+
+    def broadcast(self, data, node_id, type, sender):
+        pass
+
+
+    def create_block(self):
+        pass
+
+
+    def receive_block(self):
+        pass
+
+
+    def node_creater(self):
+        pass
+
+
+    def trx_generator(self):
+        pass
+
+    def monitoring(env):
+        pass
+
+
+    def POA(env):
+        pass
+
+go
+
+
+
+
+
