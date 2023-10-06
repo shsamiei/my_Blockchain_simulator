851b7de20b2570e816a129fd5c1b80564630310c
Hossein samiei <syhsamiei@gmail.com>
2023-10-06 16:53:03 +0330

    feat: add functions to blocks.py

---
+++ src/blocks.py
@@ -0,0 +1,20 @@
+
+
+class Block():
+    def __init__(self, size, id, trxs, node, prev_hash):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def hash_gen(self, tasks):
+        pass
+
+    def validator(self, tasks):
+        pass
+
+
+    def view_blocks(self):
+        pass
+
+    
