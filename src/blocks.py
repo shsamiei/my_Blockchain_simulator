26911a5179a5f3ea04d98c552b1bde7b6fd8ace3
Hossein samiei <syhsamiei@gmail.com>
2023-10-06 17:36:21 +0330

    feat: add hash generator logic

--- src/blocks.py
+++ src/blocks.py
@@ -1,12 +1,27 @@
+import time
+import hashlib
+
 class Block():
+
     def __init__(self, size, id, trxs, node, prev_hash):
-        pass
+        self.size = size
+        self.hash = self.hash_gen(trxs)
+        self.prev_hash = prev_hash
+        self.type = 2
+        self.generated_by = node
+        self.id = id
+        self.trxs = trxs
+        self.timestamp = time.ctime()
 
     def __repr__(self):
-        pass
+        return str(self.id)
+
 
     def hash_gen(self, tasks):
-        pass
+        re = [(task.id) for task in tasks]
+        re.sort()
+        appended_hash_ids = ','.join(map(str, re)).encode('utf-8')
+        return hashlib.sha256(appended_hash_ids).hexdigest()
 
     def validator(self, tasks):
         pass
